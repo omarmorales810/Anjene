787ecb1deeac530745ab80a65fb8a6495021fc1e
Omar Morales <omarmoralesjr@student.laverdad.edu.ph>
2022-04-21 15:47:14 +0800

    add database, make index.php dynamic

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const tiles = document.querySelector('.tiles');
+
+
+// setInterval(() => {
+  // Ajax
+  let xhr = new XMLHttpRequest(); // Creating XML object.
+  xhr.open("GET", "./php/index.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        tiles.innerHTML = data;
+      }
+    }
+  }
+  xhr.send();
+// }, 500);
