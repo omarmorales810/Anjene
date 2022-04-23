61ae7bf1955f4b1c3adfd6e0209ad155fbac441f
Omar Morales <omarmoralesjr@student.laverdad.edu.ph>
2022-04-23 15:53:18 +0800

    Add bag page

--- bag.php
+++ bag.php
@@ -1,16 +1,214 @@
 <?php require_once "./require/header.php" ?>
 
-
   <section class="bag-page">
     <div class="bag-page-container">
-      <header>Shopping bag</header>
+      <div class="bag-page-table-wrapper">
+        <header class="bag-page-header">You have 2 item in your bag</header>
+        <div class="bag-page-table">
+          <div class="bag-page-table-container">
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+            <div class="bag-page-table-item">
+              <div class="bag-page-table-item-product">
+                <img src="./img/product_img1.png" alt="dfd">
+                <div class="bag-page-table-product-content">
+                  <div class="bag-page-table-item-name">Apple Watch Series 6 GPS</div>
+                  <div class="bag-page-item-price">
+                    <span style="font-size: 0.9rem;">₱987.00</span>
+                  </div>
+              </div>
+              </div>
+              <div class="bag-page-table-item-quantity">
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn-increment quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon icon-plus-sign"><polygon points="10 4.5 5.5 4.5 5.5 0 4.5 0 4.5 4.5 0 4.5 0 5.5 4.5 5.5 4.5 10 5.5 10 5.5 5.5 10 5.5"></polygon></svg>
+                  </button>
+                  <input type="number" class="quantity-input">
+                  <button class="quantity-btn-decrement quantity-btn">
+                    <svg style="width: 12px; height: 20px" fill="#707070" enable-background="new 0 0 10 10" viewBox="0 0 10 10" x="0" y="0" class="shopee-svg-icon"><polygon points="4.5 4.5 3.5 4.5 0 4.5 0 5.5 3.5 5.5 4.5 5.5 10 5.5 10 4.5"></polygon></svg>
+                  </button>
+                </div>
+                <div class="quantity-text">Quantity</div>
+              </div>
+              <div class="bag-page-table-item-total-price">
+                <div class="item-total-price" >3, 000.00</div>
+                <div class="total-price-text">Total Price</div>
+              </div>
+              <div class="bag-page-table-item-remove">
+                <button class="item-remove-btn">Remove</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 
 
 
 
-
+   <!-- <div class="empty-bag-container">
+    <div class="empty-bag-message-container">
+      <div class="empty-bag-text">Your bag is empty</div>
+      <div class="empty-bag-message">Sign in to see if you have any saved items. Or continue shopping.</div>
+    </div>
+    <div class="bag-link-container">
+      <a href="./login.php" class="bag-link">
+        <button class="sign-in-bag-btn bag-page-btn">Sign in</button>
+      </a>
+      <a href="./index.php" class="bag-link">
+        <button class="bag-continue-shopping-btn bag-page-btn">Continue Shopping</button>
+      </a>
+    </div>
+  </div> -->
 
 
 <?php require_once "./require/footer.php" ?>
