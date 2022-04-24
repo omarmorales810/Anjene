670a32e6a1ad6311e0b885608d4d6f46afc13c6e
Omar Morales <omarmoralesjr@student.laverdad.edu.ph>
2022-04-25 03:34:45 +0800

    make something changes to bag page

--- bag.php
+++ bag.php
@@ -3,7 +3,7 @@
   <section class="bag-page">
     <header class="bag-page-header-header">Shopping Bag <svg class="bag-page-svg" xmlns="http://www.w3.org/2000/svg" style="cursor: pointer;" height="44" viewBox="0 0 13 44" width="13"><g fill="none" fill-rule="evenodd"><path d="m0 44h13v-44h-13z"/><path d="m12 25.9c0 .6-.5 1.1-1.1 1.1h-8.8c-.6 0-1.1-.5-1.1-1.1v-7.9c0-.5.5-1 1.1-1h8.9c.6 0 1.1.5 1.1 1.1v7.8zm-5.5-11.9c1.2 0 2.1.9 2.3 2h-4.6c.2-1.1 1.1-2 2.3-2zm4.4 2h-1.1c-.2-1.7-1.6-3-3.3-3s-3.1 1.3-3.3 3h-1.1c-1.2 0-2.1.9-2.1 2.1v7.9c0 1.1.9 2 2.1 2h8.9c1.1 0 2.1-.9 2.1-2.1v-7.9c-.1-1.1-1-2-2.2-2z" fill="#1d1d1f"/></g></svg></header>
     <div class="bag-page-container">
-      <div class="bag-page-header" style="visibility: hidden; pointer-events: none;">(2 Item)</div>
+      <div class="bag-page-header">(2 Item)</div>
       <div class="bag-page-table">
         
         <!-- <div class="bag-page-table-item" style="position: absolute; pointer-events: none; opacity: 0;"></div> -->
@@ -93,7 +93,7 @@
       </div>
       <div class="check-out-and-remove-all-btn-container">
         <!-- <button class="remove-all-btn">Remove All</button> -->
-        <span class="total-item-price">Total (2 Item) ₱45, 000</span>
+        <span class="total-item-price">Total ( ₱45, 000.00 )</span>
         <button class="checkout-btn">Checkout</button>
       </div>
     </div>
